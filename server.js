d38c7e63cd6996aec2740aff4a7eb944953fbee7
Divicoin <info@diviproject.org>
2017-09-29 15:24:57 -0700

    server configured

--- server.js
+++ server.js
@@ -17,3 +17,29 @@
         FILE: server.js - Global app configuration for Node/Express server
 
 *******************************************************************************/
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var app = express();
+var port = process.env.PORT || 3100;
+
+var db = require('./models');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+// require('./routes/api-routes.js)(app);
+// require('./routes/html-routes.js')(app);
+
+// Change force to true if you'd like to reset the database for any reason
+db.sequelize.sync({ force: false }).then(function() {
+      app.listen(PORT, function() {
+        console.log("App listening on PORT " + PORT);
+      });
+    });
