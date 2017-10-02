4ab5d7062578f82a71a53fe823bf703ea9253c4a
Divicoin <info@diviproject.org>
2017-10-02 16:16:08 -0700

    removed unneccesary routes

--- routes/profile-routes.js
+++
@@ -1,5 +0,0 @@
-var profileController = require('../controllers/profile-controller.js');
-
-module.exports = function(app, passport) {
-    app.get('/profile/:username?', profileController.profile);
-}
