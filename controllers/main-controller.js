bea357837dab4838b40cec1c5a658dcef28d6d1b
Divicoin <info@diviproject.org>
2017-10-05 13:16:36 -0700

    no real changes

--- controllers/main-controller.js
+++ controllers/main-controller.js
@@ -38,20 +38,28 @@ exports.explore = function(req, res) {
 }
 
 exports.profile = function(req, res) {
-    var user = {
-        username: req.user.username,
-        profile: req.params.username
-    }
+    
     db.User.findOne({
         where: {
-            username: user.profile
+            username: req.params.username
         }
     }).then(function(result) {
-        user.email = result.email;
-        res.render('profile', {user: user});
+        db.Beacon.findAll({
+            where: {
+                user_id: req.params.username
+            }
+        }).then(function(result) {
+            var user = {
+                username: req.user.username,
+                profile: req.params.username,
+                beacon: result
+            }
+            res.render('profile', {user: user});
+        })
     }).catch(function(err) {
         res.render(err)
     });
+    
 };
 
 exports.beacon = function(req, res) {
